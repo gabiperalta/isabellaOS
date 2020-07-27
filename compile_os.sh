bf246473629704fbd0bb4996fc7b4513cf068101
gabiperalta <gabiperalta16398@gmail.com>
2020-07-27 00:47:23 -0300

    add info to compile and run

--- compile_os.sh
+++ compile_os.sh
@@ -13,3 +13,7 @@ i686-elf-gcc -T linker.ld -o isabellaos.bin -ffreestanding -O2 -nostdlib boot.o 
 cp isabellaos.bin isodir/boot/isabellaos.bin
 
 grub-mkrescue -o isabellaos.iso isodir
+
+# To compile and execute
+# sh compile_os.sh
+# qemu-system-i386 -cdrom isabellaos.iso
